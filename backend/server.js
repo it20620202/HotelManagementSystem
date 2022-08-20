72dcfbc4bbcf8836d20bb64d480def085e75e16a
IT20620202 <89015836+it20620202@users.noreply.github.com>
2022-08-20 20:16:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,86 +25,17 @@ connection.once("open", () => {
   console.log("Mongodb Connection success!");
 });
 
-// //login route
-// const userRoutes = require("./routes/users.js");
-// app.use("/user", userRoutes);
+
 
 // //routesNoticeBoard
 // const noticeRouter = require("./routes/notice");
 // app.use("/notice", noticeRouter);
-// //routesSubject
-// const subjectRouter = require("./routes/subject");
-// app.use("/subject", subjectRouter);
 
-// const matRouter = require("./routes/LecturerSubjects.js");
-// app.use("/material",matRouter);
+//Food
+const foodRouter = require("./routes/FoodRoute.js");
+app.use("/food",foodRouter);
 
-// //routesStaff
-// //import route
-// const staffRouter = require("./routes/staffRoute.js");
 
-// //route middleware
-// app.use("/Staff",staffRouter);
-
-// //routesTickets
-// const ticketRouter = require("./routes/tickets");
-// app.use("/ticket", ticketRouter);
-
-// //exams
-// const studentRouter = require("./routes/exams.js");
-// const lectRouter = require("./routes/lecturers.js");
-// const courseRouter = require("./routes/course.js");
-// const attemptRouter = require("./routes/examAttempts.js");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-// const fdRouter = require("./routes/feedbackA");
-// app.use("/exams", studentRouter);
-// app.use("/lects", lectRouter);
-// app.use("/course", courseRouter);
-// app.use("/attempts", attemptRouter); app.use("/fd", fdRouter);
-
-// //routes classroom
-// const classroomRouter = require("./routes/classrooms");
-// app.use("/classroom", classroomRouter);
-
-// //routes Attendance
-// const attendanceRouter = require("./routes/attendances.js")
-// app.use("/attendance",attendanceRouter);
-
-// //routesPayments
-// const studentRegPaymentsRouter = require("./routes/studentRegistrationPayments");
-// const studentSubjectPaymentsRouter = require("./routes/studentSubjectPayments");
-// const regPaymentsRouter = require("./routes/registrationPayments");
-// const subjectPaymentsRouter = require("./routes/subjectPayments");
-
-// app.use("/studentRegistrationPayment", studentRegPaymentsRouter);
-// app.use("/studentSubjectPayment", studentSubjectPaymentsRouter);
-// app.use("/registrationPayments", regPaymentsRouter);
-// app.use("/subjectPayments", subjectPaymentsRouter);
-
-// //routes add student
-// const addStudent = require("./routes/addStudent");
-// const student = require("./routes/student");
-
-// app.post("/addStudent", addStudent);
-// app.get("/student", student);
-// app.get("/editStudent", student);
-// app.get("/searchStudent", student);
-// app.post("/updateStudent", student);
-// app.post("/deleteStudent", student);
-
-// const downloadManager = require("./routes/downloadManager");
-// app.get("/download", downloadManager);
-
-// const studentReport = require("./routes/studentReport");
-// app.get("/studentReport", studentReport);
-
-// /*const lectRouter = require("./routes/lecturers.js");
-// app.use("/lects", lectRouter);*/
-
-// /*const courseRouter = require("./routes/courses.js");
-// app.use("/courses", courseRouter);*/
-
-// /*const attemptRouter = require("./routes/examAttempts.js");
-// app.use("/attempts", attemptRouter);*/
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port number: ${PORT}`);
